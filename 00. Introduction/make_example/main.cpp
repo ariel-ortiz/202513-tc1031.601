3dc38cd60db6a01988dd6b68d4fdc07fa00b5b4c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-08-15 19:00:14 +0000

    Create files to demo use of Makefile

---
+++ 00. Introduction/make_example/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "fact.h"
+
+int main()
+{
+    int x = 5;
+    int r = factorial(x);
+    std::cout << "Hello\n";
+    std::cout << x << "! = " << r << "\n";
+    return 0;
+}
