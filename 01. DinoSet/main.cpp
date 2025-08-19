e2eae0b2ae800d73819975d7a933e0023786aae5
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-08-19 19:03:27 +0000

    Add initial source code for DinoSet lab

---
+++ 01. DinoSet/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "dinoset.h"
+
+int main()
+{
+    DinoSet d;
+    d.add(DinoId::velociraptor);
+    d.add(DinoId::tyrannosaurus);
+    std::cout << d.to_string() << "\n";
+    return 0;
+}
