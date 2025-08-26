85ea6ee58f8683437dcf5f75b29e63c60882b032
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-08-26 19:00:54 +0000

    Add code to implement + operator

--- 01. DinoSet/main.cpp
+++ 01. DinoSet/main.cpp
@@ -3,9 +3,14 @@
 
 int main()
 {
-    DinoSet d;
-    d.add(DinoId::velociraptor);
-    d.add(DinoId::tyrannosaurus);
-    std::cout << d.to_string() << "\n";
+    DinoSet a, b;
+    a.add(DinoId::velociraptor);
+    a.add(DinoId::pteranodon);
+    b.add(DinoId::velociraptor);
+    b.add(DinoId::tyrannosaurus);
+    DinoSet c = a + b;
+    std::cout << "a = " << a.to_string() << "\n";
+    std::cout << "b = " << b.to_string() << "\n";
+    std::cout << "c = " << c.to_string() << "\n";
     return 0;
 }
