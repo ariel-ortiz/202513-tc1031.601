21cd7424add08529e3570a7f8d01ecb91ffb6b36
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-11-14 17:56:14 +0000

    Add code to solve sum, duplicate, and last

--- 10. Recursion/recursion/main.cpp
+++ 10. Recursion/recursion/main.cpp
@@ -4,8 +4,11 @@
 
 int main()
 {
-    IntList a {5, 1, 5, 1};
+    IntList a {5, 1, 5, 1, 99};
     std::cout << size(a) << "\n";
     std::cout << cons_end(100, a) << "\n";
+    std::cout << sum(a) << "\n";
+    std::cout << duplicate(a) << "\n";
+    std::cout << last(a) << "\n";
     return 0;
 }
