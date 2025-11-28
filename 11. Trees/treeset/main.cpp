7895cf7d7d24ce97bd32dc2b861dc2624cb3cb21
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-11-28 18:47:50 +0000

    Add code for destructor, levelorder, height, is_full, leaf_count, is_perfect

--- 11. Trees/treeset/main.cpp
+++ 11. Trees/treeset/main.cpp
@@ -30,11 +30,23 @@ int main()
     std::cout << "\n";
     a.inorder(add_to_vector<char>);
     std::cout << global_char_vector[2] << "\n";
-    TreeSet<int> b {10, 7, 15, 9, 3, 17, 12};
+    TreeSet<int> b {11, 7, 15, 9, 3, 17, 12};
     b.inorder(print_something<int>);
     std::cout << "\n";
     b.inorder(add_global_int_accum);
     std::cout << global_int_accum << "\n";
+    a.levelorder(print_something<char>);
+    std::cout << "\n";
+    b.levelorder(print_something<int>);
+    std::cout << "\n";
+    std::cout << "Is full? " << b.is_full() << "\n";
+    std::cout << "Is Perfect? " << b.is_perfect() << "\n";
+    b.add(8);
+    std::cout << "Is full? " << b.is_full() << "\n";
+    b.add(10);
+    std::cout << "Is full? " << b.is_full() << "\n";
+    std::cout << "Leaf Count " << b.leaf_count() << "\n";
+    std::cout << "Is Perfect? " << b.is_perfect() << "\n";
 
     return 0;
 }
--- 11. Trees/treeset/tests.cpp
+++ 11. Trees/treeset/tests.cpp
@@ -157,73 +157,73 @@ TEST_CASE("test the TreeSet class")
             15, 14, 12, 8} == vi4);
     }
 
-    // SECTION("test levelorder") {
-    //     std::vector<std::string> vs1, vs2, vs3, vs4;
-    //     std::vector<int> vi1, vi2, vi3, vi4;
-    //     a.levelorder([&] (std::string s) { vs1.push_back(s); });
-    //     REQUIRE(std::vector<std::string> {} == vs1);
-    //     b.levelorder([&] (std::string s) { vs2.push_back(s); });
-    //     REQUIRE(std::vector<std::string> {"a"} == vs2);
-    //     c.levelorder([&] (std::string s) { vs3.push_back(s); });
-    //     REQUIRE(std::vector<std::string> {"m", "g", "r", "a", "k",
-    //         "p", "z"} == vs3);
-    //     d.levelorder([&] (std::string s) { vs4.push_back(s); });
-    //     REQUIRE(std::vector<std::string> {"d", "c", "e", "b", "f",
-    //         "a", "g"} == vs4);
-    //     e.levelorder([&] (int i) { vi1.push_back(i); });
-    //     REQUIRE(std::vector<int> {22, 10, 30, 5, 15, 25, 35, 12, 17,
-    //         33, 40, 16, 19, 18, 20} == vi1);
-    //     f.levelorder([&] (int i) { vi2.push_back(i); });
-    //     REQUIRE(std::vector<int> {8, 1, 7, 2, 6, 3, 5, 4} == vi2);
-    //     g.levelorder([&] (int i) { vi3.push_back(i); });
-    //     REQUIRE(std::vector<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-    //         12, 13, 14, 15} == vi3);
-    //     h.levelorder([&] (int i) { vi4.push_back(i); });
-    //     REQUIRE(std::vector<int> {8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7,
-    //         9, 11, 13, 15} == vi4);
-    // }
+    SECTION("test levelorder") {
+        std::vector<std::string> vs1, vs2, vs3, vs4;
+        std::vector<int> vi1, vi2, vi3, vi4;
+        a.levelorder([&] (std::string s) { vs1.push_back(s); });
+        REQUIRE(std::vector<std::string> {} == vs1);
+        b.levelorder([&] (std::string s) { vs2.push_back(s); });
+        REQUIRE(std::vector<std::string> {"a"} == vs2);
+        c.levelorder([&] (std::string s) { vs3.push_back(s); });
+        REQUIRE(std::vector<std::string> {"m", "g", "r", "a", "k",
+            "p", "z"} == vs3);
+        d.levelorder([&] (std::string s) { vs4.push_back(s); });
+        REQUIRE(std::vector<std::string> {"d", "c", "e", "b", "f",
+            "a", "g"} == vs4);
+        e.levelorder([&] (int i) { vi1.push_back(i); });
+        REQUIRE(std::vector<int> {22, 10, 30, 5, 15, 25, 35, 12, 17,
+            33, 40, 16, 19, 18, 20} == vi1);
+        f.levelorder([&] (int i) { vi2.push_back(i); });
+        REQUIRE(std::vector<int> {8, 1, 7, 2, 6, 3, 5, 4} == vi2);
+        g.levelorder([&] (int i) { vi3.push_back(i); });
+        REQUIRE(std::vector<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+            12, 13, 14, 15} == vi3);
+        h.levelorder([&] (int i) { vi4.push_back(i); });
+        REQUIRE(std::vector<int> {8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7,
+            9, 11, 13, 15} == vi4);
+    }
 
-    // SECTION("test height") {
-    //     REQUIRE(-1 == a.height());
-    //     REQUIRE(0 == b.height());
-    //     REQUIRE(2 == c.height());
-    //     REQUIRE(3 == d.height());
-    //     REQUIRE(5 == e.height());
-    //     REQUIRE(7 == f.height());
-    //     REQUIRE(14 == g.height());
-    //     REQUIRE(3 == h.height());
-    // }
+    SECTION("test height") {
+        REQUIRE(-1 == a.height());
+        REQUIRE(0 == b.height());
+        REQUIRE(2 == c.height());
+        REQUIRE(3 == d.height());
+        REQUIRE(5 == e.height());
+        REQUIRE(7 == f.height());
+        REQUIRE(14 == g.height());
+        REQUIRE(3 == h.height());
+    }
 
-    // SECTION("test is_full") {
-    //     REQUIRE(a.is_full());
-    //     REQUIRE(b.is_full());
-    //     REQUIRE(c.is_full());
-    //     REQUIRE_FALSE(d.is_full());
-    //     REQUIRE(e.is_full());
-    //     REQUIRE_FALSE(f.is_full());
-    //     REQUIRE_FALSE(g.is_full());
-    //     REQUIRE(h.is_full());
-    // }
+    SECTION("test is_full") {
+        REQUIRE(a.is_full());
+        REQUIRE(b.is_full());
+        REQUIRE(c.is_full());
+        REQUIRE_FALSE(d.is_full());
+        REQUIRE(e.is_full());
+        REQUIRE_FALSE(f.is_full());
+        REQUIRE_FALSE(g.is_full());
+        REQUIRE(h.is_full());
+    }
 
-    // SECTION("test leaf_count") {
-    //     REQUIRE(0 == a.leaf_count());
-    //     REQUIRE(1 == b.leaf_count());
-    //     REQUIRE(4 == c.leaf_count());
-    //     REQUIRE(2 == d.leaf_count());
-    //     REQUIRE(8 == e.leaf_count());
-    //     REQUIRE(1 == f.leaf_count());
-    //     REQUIRE(1 == g.leaf_count());
-    //     REQUIRE(8 == h.leaf_count());
-    // }
+    SECTION("test leaf_count") {
+        REQUIRE(0 == a.leaf_count());
+        REQUIRE(1 == b.leaf_count());
+        REQUIRE(4 == c.leaf_count());
+        REQUIRE(2 == d.leaf_count());
+        REQUIRE(8 == e.leaf_count());
+        REQUIRE(1 == f.leaf_count());
+        REQUIRE(1 == g.leaf_count());
+        REQUIRE(8 == h.leaf_count());
+    }
 
-    // SECTION("test is_perfect") {
-    //     REQUIRE(a.is_perfect());
-    //     REQUIRE(b.is_perfect());
-    //     REQUIRE(c.is_perfect());
-    //     REQUIRE_FALSE(d.is_perfect());
-    //     REQUIRE_FALSE(e.is_perfect());
-    //     REQUIRE_FALSE(f.is_perfect());
-    //     REQUIRE_FALSE(g.is_perfect());
-    //     REQUIRE(h.is_perfect());
-    // }
+    SECTION("test is_perfect") {
+        REQUIRE(a.is_perfect());
+        REQUIRE(b.is_perfect());
+        REQUIRE(c.is_perfect());
+        REQUIRE_FALSE(d.is_perfect());
+        REQUIRE_FALSE(e.is_perfect());
+        REQUIRE_FALSE(f.is_perfect());
+        REQUIRE_FALSE(g.is_perfect());
+        REQUIRE(h.is_perfect());
+    }
 }
